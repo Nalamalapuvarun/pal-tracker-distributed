935448c59a865be6d70d0138f765d3b74350384d
Nalamalapuvarun <varun.nalamalapu@gmail.com>
2019-03-08 10:29:58 -0500

    added the command back

--- databases/create_databases.sql
+++ databases/create_databases.sql
@@ -14,7 +14,8 @@ DROP DATABASE IF EXISTS tracker_registration_test;
 
 DROP DATABASE IF EXISTS tracker_timesheets_test;
 
-
+CREATE USER IF NOT EXISTS 'tracker'@'localhost'
+  identified by '';
 GRANT ALL PRIVILEGES ON *.* TO 'tracker' @'localhost';
 
 
